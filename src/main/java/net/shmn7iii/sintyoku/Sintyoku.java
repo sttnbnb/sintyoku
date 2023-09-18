5d126f7bc869a2ffeeb3f57186bee7d69d0f6511
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-09-18 18:16:53 +0900

    Init project

---
+++ src/main/java/net/shmn7iii/sintyoku/Sintyoku.java
@@ -0,0 +1,17 @@
+package net.shmn7iii.sintyoku;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Sintyoku extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
